7316d14580f326d94a818920918013360a2bca8b
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2024-11-10 08:00:31 +0530

    removed unwanted files

--- client/app/(root)/(home)/scan/page.tsx
+++ client/app/(root)/(home)/scan/page.tsx
@@ -4,12 +4,18 @@ import dynamic from "next/dynamic";
 import { useState } from "react";
 import "./page.css";
 
-const VideoCapture = dynamic(() => import("@/components/shared/VideoCapture/VideoCapture"), {
-  ssr: false,
-});
-const ImageCapture = dynamic(() => import("@/components/shared/ImageCapture/ImageCapture"), {
-  ssr: false,
-});
+const VideoCapture = dynamic(
+  () => import("@/components/shared/VideoCapture/VideoCapture"),
+  {
+    ssr: false,
+  }
+);
+const ImageCapture = dynamic(
+  () => import("@/components/shared/ImageCapture/ImageCapture"),
+  {
+    ssr: false,
+  }
+);
 
 enum Modes {
   VIDEO_CAPTURE = "video",
@@ -24,13 +30,11 @@ export default function Home() {
 
   return (
     <div className="hello-world-page">
-      <div className="title">
-        <h2 className="title-text">Hello World for Next.js</h2>
-      </div>
       <div className="buttons-container">
         <button
           style={{
-            backgroundColor: mode === Modes.VIDEO_CAPTURE ? "rgb(255,174,55)" : "white",
+            backgroundColor:
+              mode === Modes.VIDEO_CAPTURE ? "rgb(255,174,55)" : "white",
           }}
           onClick={showVideoCapture}
         >
@@ -38,14 +42,17 @@ export default function Home() {
         </button>
         <button
           style={{
-            backgroundColor: mode === Modes.IMAGE_CAPTURE ? "rgb(255,174,55)" : "white",
+            backgroundColor:
+              mode === Modes.IMAGE_CAPTURE ? "rgb(255,174,55)" : "white",
           }}
           onClick={showImageCapture}
         >
           Decode Image
         </button>
       </div>
-      <div className="container">{mode === Modes.VIDEO_CAPTURE ? <VideoCapture /> : <ImageCapture />}</div>
+      <div className="container">
+        {mode === Modes.VIDEO_CAPTURE ? <VideoCapture /> : <ImageCapture />}
+      </div>
     </div>
   );
 }
